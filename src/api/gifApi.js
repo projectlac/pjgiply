7e143000ba5916a16c5518087a9721ecd0e1cb07
projeclac <dft1711198@gmail.com>
2022-02-12 11:42:35 +0700

    comment

--- src/api/gifApi.js
+++ src/api/gifApi.js
@@ -1,17 +1,16 @@
 import axiosClient from "./axiosClient";
 const api_key = process.env.REACT_APP_API_KEY;
 const gifApi = {
-  treding() {
-
+  treding() { // Use API to get trending gifs
     return axiosClient.get(`/gifs/trending?api_key=${api_key}&limit=30`);
   },
-  autocomple(input) {
+  autocomple(input) { // Use API to get search hint
     return axiosClient.get(`/gifs/search/tags?api_key=${api_key}&q=${input}&limit=30`);
   },
-  search(input) {
+  search(input) { // Use API to search gifs
     return axiosClient.get(`/gifs/search?api_key=${api_key}&q=${input}&limit=30`);
   },
-  getImgById(input) {
+  getImgById(input) { // Use API to get gifs by id
     return axiosClient.get(`/gifs?api_key=${api_key}&ids=${input}`)
   }
 };
